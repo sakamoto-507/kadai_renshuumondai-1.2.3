7ee69a75c4fb6f49855aec94d1f294804e0e5698
SakamotoChisato <241127@std.hi-joho.ac.jp>
2025-05-21 09:55:43 +0900

    java練習問題15~27+a

--- src/Review25.java
+++ src/Review25.java
@@ -1,7 +1,10 @@
 public class Review25 {
     public static void main(String[] args) {
-        numArray;
-        numArray = {11,12,13,14,15}
-
+        int numArray[] = {11,12,13,14,15};
+        int sum = 0;
+        for (int i = 0;i < numArray.length;i++){
+            sum = sum + numArray[i];
+        }
+        System.out.print("結果: " + sum);
     }
 }
---
+++ src/Review26.java
@@ -0,0 +1,14 @@
+public class Review26 {
+    public static void main(String[] args) {
+        int numArray[] = {11,12,13,14,14,14,15};
+        int target = 14;
+        int count = 0;
+        for (int i = 0; i < numArray.length; i++) {
+            if (numArray[i] == target) {
+                count = count +1;
+            }
+        }
+        System.out.println("探索した値: " + target);
+        System.out.println("一致した数: " + count);
+    }
+}
